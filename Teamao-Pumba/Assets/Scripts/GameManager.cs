26a8b714fc0e5e46b75303d55f0adcff69e829e7
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2019-08-22 14:43:53 -0300

    Varios Bagulhos
    Arena+Menu+Settings

---
+++ Teamao-Pumba/Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Players;
+    public GameObject Settings;
+    public GameObject ErrorText;
+    private bool PlayersSelected;
+    public void OnePlayer() {
+        Players.transform.GetChild(0).gameObject.SetActive(true);
+        Players.transform.GetChild(1).gameObject.SetActive(false);
+        Players.transform.GetChild(2).gameObject.SetActive(false);
+        Players.transform.GetChild(3).gameObject.SetActive(false);
+        PlayersSelected = true;
+    }
+     public void TwoPlayer() {
+        Players.transform.GetChild(0).gameObject.SetActive(true);
+        Players.transform.GetChild(1).gameObject.SetActive(true);
+        Players.transform.GetChild(2).gameObject.SetActive(false);
+        Players.transform.GetChild(3).gameObject.SetActive(false);
+        PlayersSelected = true;
+    }
+     public void ThreePlayer() {
+        Players.transform.GetChild(0).gameObject.SetActive(true);
+        Players.transform.GetChild(1).gameObject.SetActive(true);
+        Players.transform.GetChild(2).gameObject.SetActive(true);
+        Players.transform.GetChild(3).gameObject.SetActive(false);
+        PlayersSelected = true;
+    }
+     public void FourPlayer() {
+        Players.transform.GetChild(0).gameObject.SetActive(true);
+        Players.transform.GetChild(1).gameObject.SetActive(true);
+        Players.transform.GetChild(2).gameObject.SetActive(true);
+        Players.transform.GetChild(3).gameObject.SetActive(true);
+        PlayersSelected = true;
+    }
+    public void PlayGame() {
+        if(PlayersSelected) {
+            Settings.SetActive(false);
+        }
+        else {
+            ErrorText.SetActive(true);
+        }
+        
+    }
+
+
+    void Start()
+    {
+        PlayersSelected = false;
+        Settings.SetActive(true);
+        Players.transform.GetChild(0).gameObject.SetActive(false);
+        Players.transform.GetChild(1).gameObject.SetActive(false);
+        Players.transform.GetChild(2).gameObject.SetActive(false);
+        Players.transform.GetChild(3).gameObject.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Teamao-Pumba/Assets/Scripts/SceneJumper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneJumper : MonoBehaviour
+{
+    public void GoArena() {
+        SceneManager.LoadScene("Arena",LoadSceneMode.Single);
+    }
+    public void GoCredits() {
+        SceneManager.LoadScene("Credits",LoadSceneMode.Single);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+   
+}
