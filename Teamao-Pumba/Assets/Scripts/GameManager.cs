efe2ba45d8582f2376bb6c3d058202e405434832
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2019-08-24 23:11:13 -0300

    Tela de character select meio pronta

--- Teamao-Pumba/Assets/Scripts/GameManager.cs
+++ Teamao-Pumba/Assets/Scripts/GameManager.cs
@@ -1,49 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+/*Script para gerenciamento do jogo 
 
+Author: Vinny*/
 public class GameManager : MonoBehaviour
 {
-    public GameObject Players;
-    public GameObject Settings;
-    public GameObject ErrorText;
+    public GameObject Players; // Os jogadores
+    public GameObject HowManyPlayers; // O canvas contendo quantos jogadores
+    public GameObject CharacterSelect; // O canvas contendo a seleção de personagem
+    public Text ErrorText; // Um texto de erro caso o jogo comece sem escolher a quantidade de jogadores
     private bool PlayersSelected;
-     public void TwoPlayer() {
+     public void TwoPlayer() { // função para dois players
         Players.transform.GetChild(0).gameObject.SetActive(true);
         Players.transform.GetChild(1).gameObject.SetActive(true);
         Players.transform.GetChild(2).gameObject.SetActive(false);
         Players.transform.GetChild(3).gameObject.SetActive(false);
         PlayersSelected = true;
     }
-     public void ThreePlayer() {
+     public void ThreePlayer() { // função para três players
         Players.transform.GetChild(0).gameObject.SetActive(true);
         Players.transform.GetChild(1).gameObject.SetActive(true);
         Players.transform.GetChild(2).gameObject.SetActive(true);
         Players.transform.GetChild(3).gameObject.SetActive(false);
         PlayersSelected = true;
     }
-     public void FourPlayer() {
+     public void FourPlayer() { // função para quatro players
         Players.transform.GetChild(0).gameObject.SetActive(true);
         Players.transform.GetChild(1).gameObject.SetActive(true);
         Players.transform.GetChild(2).gameObject.SetActive(true);
         Players.transform.GetChild(3).gameObject.SetActive(true);
         PlayersSelected = true;
     }
-    public void PlayGame() {
+    public void Next() { // Vai para a tela de seleção de personagem
         if(PlayersSelected) {
-            Settings.SetActive(false);
+            HowManyPlayers.SetActive(false);
+            CharacterSelect.SetActive(true);
         }
         else {
-            ErrorText.SetActive(true);
+            ErrorText.text = "Selecione o número de jogadores antes de começar a partida!";
         }
-        
+    }
+    public void Previous() { // Volta para a tela de numero de jogadores
+        PlayersSelected = false;
+        HowManyPlayers.SetActive(true);
+        CharacterSelect.SetActive(false);
+    }
+    public void PlayGame() { // Começa o jogo
+        CharacterSelect.transform.parent.gameObject.SetActive(false);
     }
 
 
     void Start()
     {
         PlayersSelected = false;
-        Settings.SetActive(true);
+        HowManyPlayers.SetActive(true);
+        CharacterSelect.SetActive(false);
         Players.transform.GetChild(0).gameObject.SetActive(false);
         Players.transform.GetChild(1).gameObject.SetActive(false);
         Players.transform.GetChild(2).gameObject.SetActive(false);
@@ -54,7 +67,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         if(PlayersSelected) {
-            ErrorText.SetActive(false);
+            ErrorText.text = "";
         }
     }
 }
