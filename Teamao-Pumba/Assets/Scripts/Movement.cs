034270a79166ef60f1d9f9001e75d1a1bb8e1019
Raphael Ponce <raphafleury123@gmail.com>
2019-08-28 17:55:03 -0300

    Adicionando cena de testes e adcionando movimentação para o player02

--- Teamao-Pumba/Assets/Scripts/Movement.cs
+++ Teamao-Pumba/Assets/Scripts/Movement.cs
@@ -6,6 +6,10 @@ public class Movement : MonoBehaviour
 {
     public float movementSpeed;
 
+    public GameObject player;
+
+
+
     // Use this for initialization
     void Start()
     {
@@ -15,27 +19,70 @@ public class Movement : MonoBehaviour
     //Update is called once per frame
     void FixedUpdate()
     {
+        Player01Movement("Player01");
+        Player02Movement("Player02");
 
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w"))
-        {
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
-        }
-        else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift))
-        {
-            transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
-        }
-        else if (Input.GetKey("s"))
-        {
-            transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
-        }
 
-        if (Input.GetKey("a") && !Input.GetKey("d"))
-        {
-            transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
-        }
-        else if (Input.GetKey("d") && !Input.GetKey("a"))
-        {
-            transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
-        }
     }
+
+    void Player01Movement(string playerName)
+    {
+        if (player.gameObject.name == playerName)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey("w"))
+            {
+                transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+            }
+            else if (Input.GetKey("w") && !Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey("s"))
+            {
+                transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+            }
+
+            if (Input.GetKey("a") && !Input.GetKey("d"))
+            {
+                transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey("d") && !Input.GetKey("a"))
+            {
+                transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+            }
+        }
+
+    }
+
+    void Player02Movement(string playerName)
+    {
+        if (player.gameObject.name == playerName)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed * 2.5f;
+            }
+            else if (Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+            }
+        }
+
+    }
+
+
+
 }
