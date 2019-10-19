1b3a1ad2f0490a34bca4f695baefea4058a2506f
Nagib Suaid <nagibmoura@gmail.com>
2019-10-19 00:15:07 -0300

    Despawn de itens

---
+++ Teamao-Pumba/Assets/ItemScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public float despawnTime;
+    void Start()
+    {
+        StartCoroutine(DestroyThis());
+    }
+
+    IEnumerator DestroyThis()
+    {
+        yield return new WaitForSeconds(despawnTime);
+        Destroy(this.gameObject);
+    }
+}
