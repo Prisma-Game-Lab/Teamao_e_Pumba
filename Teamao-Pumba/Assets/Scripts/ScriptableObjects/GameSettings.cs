38f018c5344d6aad24a5de41f985e5bfcd8dac72
Raphael Ponce <raphafleury123@gmail.com>
2019-11-12 22:18:41 -0300

    implementação do scriptable object das settings

---
+++ Teamao-Pumba/Assets/Scripts/ScriptableObjects/GameSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Game Settngs", menuName = "GameSettings")]
+public class GameSettings : ScriptableObject
+{
+    public int playerNumbers;
+}
