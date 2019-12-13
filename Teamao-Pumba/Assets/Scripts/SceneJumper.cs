d495354398cf4886cf4ebb0ded5c083740b48a27
mlhomsi <mlhomsi@gmail.com>
2019-12-13 06:19:56 -0300

    Countdown pra partida

--- Teamao-Pumba/Assets/Scripts/SceneJumper.cs
+++ Teamao-Pumba/Assets/Scripts/SceneJumper.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 /*Script Feito para fazer funções de troca de cena
 
 
-Author: Vinny */
+Author: Vinny
+Homsi brincou também*/
 public class SceneJumper : MonoBehaviour
 {
+    public AudioSource Arena;
+
     public void GoArena()
     {
+        Arena.enabled = true;
         SceneManager.LoadScene("Arena", LoadSceneMode.Single);
         Time.timeScale = 1f;
     }
@@ -19,11 +23,13 @@ public class SceneJumper : MonoBehaviour
     }
     public void GoMenu()
     {
+        Arena.enabled = false;
         SceneManager.LoadScene("Menu", LoadSceneMode.Single);
         Time.timeScale = 1f;
     }
     public void QuitGame()
     {
+        Arena.enabled = false;
         Application.Quit();
     }
     public void Reload()
