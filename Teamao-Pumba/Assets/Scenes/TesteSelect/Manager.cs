3d00bca69a2c0f50143bab5e75bdbeada7047f19
Raphael Ponce <raphafleury123@gmail.com>
2019-12-17 22:17:32 -0300

    Character Select Teste

---
+++ Teamao-Pumba/Assets/Scenes/TesteSelect/Manager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public GameObject player;
+    [SerializeField] public List<GameObject> timeList = new List<GameObject>();
+
+    private int selectedTime;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            LeftArrow();
+        }
+    }
+
+    public void LeftArrow()
+    {
+        selectedTime--;
+        if (selectedTime < 0)
+        {
+            selectedTime = timeList.Count - 1;
+        }
+
+
+    }
+
+    public void RightArrow()
+    {
+        selectedTime++;
+        if (selectedTime == timeList.Count)
+        {
+            selectedTime = 0;
+        }
+
+    }
+}
